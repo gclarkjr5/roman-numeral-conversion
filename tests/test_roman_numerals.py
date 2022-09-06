b00868341103a900ecdba26b31e9b56524f82bdc
Gary Clark <gclark@xccelerated.io>
2022-09-06 19:26:09 +0200

    some refactoring to get stuff to work

--- tests/test_roman_numerals.py
+++ tests/test_roman_numerals.py
@@ -1,57 +1,57 @@
-from conversion.roman_numerals import convert
+from roman_numerals_conversion.roman_numerals import convert_roman_2_int, convert_int_2_roman
 
 
 def test_one():
     expected = 1
-    actual = convert("I")
+    actual = convert_roman_2_int("I")
 
     assert expected == actual
 
 
 def test_three():
     expected = 3
-    actual = convert("III")
+    actual = convert_roman_2_int("III")
 
     assert expected == actual
 
 
 def test_four():
     expected = 4
-    actual = convert("IV")
+    actual = convert_roman_2_int("IV")
 
     assert expected == actual
 
 
 def test_nineteen():
     expected = 19
-    actual = convert("XIX")
+    actual = convert_roman_2_int("XIX")
 
     assert expected == actual
 
 
 def test_fournine():
     expected = 49
-    actual = convert("XLIX")
+    actual = convert_roman_2_int("XLIX")
 
     assert expected == actual
 
 
 def test_onesevennine():
     expected = 179
-    actual = convert("CLXXIX")
+    actual = convert_roman_2_int("CLXXIX")
 
     assert expected == actual
 
 
 def test_twozerosixseven():
     expected = 2067
-    actual = convert("MMLXVII")
+    actual = convert_roman_2_int("MMLXVII")
 
     assert expected == actual
 
 
 def test_threeoneonenine():
     expected = 3119
-    actual = convert("MMMCXIX")
+    actual = convert_roman_2_int("MMMCXIX")
 
     assert expected == actual
